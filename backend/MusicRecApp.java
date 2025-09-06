e21b29c22bd4e6ca25952d0cb7afe3b94445b291
LeenA-hub <almutileen7@gmail.com>
2025-09-06 14:56:54 -0400

    add those changes

---
+++ backend/MusicRecApp.java
@@ -0,0 +1,16 @@
+package backend;
+
+public
+package backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRecApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MusicRecApp.class, args);
+    }
+}{
+
+}
