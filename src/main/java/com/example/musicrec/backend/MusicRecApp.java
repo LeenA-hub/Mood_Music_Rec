133475252dab77f58a6e3a28a85b955301a2c646
LeenA-hub <almutileen7@gmail.com>
2025-09-06 15:13:37 -0400

    ayayayayya it's runnign mvn worked

---
+++ src/main/java/com/example/musicrec/backend/MusicRecApp.java
@@ -0,0 +1,11 @@
+package com.example.musicrec.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRecApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MusicRecApp.class, args);
+    }
+}
